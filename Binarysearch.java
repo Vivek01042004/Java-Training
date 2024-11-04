b3e823f122913a1f0d4c78ac990bd7c849760cbf
Vivek01042004 <163495347+Vivek01042004@users.noreply.github.com>
2024-11-04 10:25:28 +0530

    Update Binarysearch.java

--- Binarysearch.java
+++ Binarysearch.java
@@ -3,25 +3,25 @@ public class Binarysearch{
     public static void main(String [] args){
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
-        int s=scanner.nextInt();
-        int arr[]=new int[s];
+        int n=scanner.nextInt();
+        int arr[]=new int[n];
         System.out.print("Enter the elements of the array:");
-        for(int i=0;i<s;i++){
+        for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
-        int key=7,low=0,high=arr.length-1,t=0;
-            while (low <= high) {
-                int mid = (low + high) / 2;
+        int key=7,s=0,e=arr.length-1,t=0;
+            while (s <= e) {
+                int mid = (s + e) / 2;
                 if (arr[mid] == key) {
-                    t=1; 
+                    t++; 
                     break;
                 } else if (arr[mid] < key) {
-                    low = mid + 1; 
+                    s = mid + 1; 
                 } else {
-                    high = mid - 1; 
+                    e = mid - 1; 
                 }
             }
-        if(t==1) System.out.println("The element is present in the array");
+        if(t>0) System.out.println("The element is present in the array");
         else System.out.println("The element is not present in the array");  
 }
 }
